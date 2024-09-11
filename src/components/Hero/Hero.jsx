24b51350fe668ae39b8bebb7648c74bdbe2d7434
Gowshikkumar99 <160531318+Gowshikkumar99@users.noreply.github.com>
2024-09-12 00:38:11 +0530

    Updated the images

--- src/components/Hero/Hero.jsx
+++ src/components/Hero/Hero.jsx
@@ -1,21 +1,18 @@
 import React from 'react';
-import Banner from '../../assets/images/Banner.png';
-
 
 export default function Hero() {
     return (
         <div className="hero">
             <div className="hero__sub-text">FURNITURE STORE</div>
             <div className="hero__heading">
-            Discover the Artistry of Modern Contemporary Furniture
+                Discover the Artistry of Modern Contemporary Furniture
             </div>
             <div className="hero__description">
-            Experience the elegance and functionality of cutting-edge design where luxury meets innovation in every piece for ultimate relaxation
+                Experience the elegance and functionality of cutting-edge design where luxury meets innovation in every piece for ultimate relaxation
             </div>
             <div className="hero__banner">
-                <img src={Banner} alt='hero'/>
+                <img src="/assets/images/Banner.png" alt="hero" />
             </div>
         </div>    
-    )
+    );
 }
-
